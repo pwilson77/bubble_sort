16b0483c52a2a3100bd04671c3b30d0494af7a40
pwilson77 <p.o.yirenkyi@gmail.com>
2019-04-08 22:06:06 +0000

    made changes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,21 +1,20 @@
-
 def bubble_sort(arr)
-    i = 0
-    n = arr.length
-    while i < n 
-        limit = n-i-1
-        for j in 0..limit
-            arr[j+1] == nil ? break : "continue"
-            if (arr[j+1] < arr[j])
-                small = arr[j + 1]
-                large = arr[j]
-                arr[j] = small
-                arr[j + 1] = large
-            end
+  i = 0
+  n = arr.length
+  while i < n 
+    limit = n-i-1
+    for j in 0..limit
+        arr[j+1] == nil ? break : "continue"
+        if (arr[j+1] < arr[j])
+            small = arr[j + 1]
+            large = arr[j]
+            arr[j] = small
+            arr[j + 1] = large
         end
-        i+=1
-    end    
-     arr
+    end
+    i+=1
+  end    
+  arr
 end
 
 arr = [4,3,78,2,0,2]
