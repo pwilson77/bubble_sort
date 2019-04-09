13555227700458d9a77c4eb8afcea73d60e02132
pwilson77 <p.o.yirenkyi@gmail.com>
2019-04-09 09:33:57 +0000

    done refactoring

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,20 +3,17 @@ def bubble_sort(arr)
   while i < arr.length
     limit = arr.length - i - 1
     for j in 0..limit
-        break if arr[j+1].nil?
-        if (arr[j+1] < arr[j])
-            small = arr[j + 1]
-            large = arr[j]
-            arr[j] = small
-            arr[j + 1] = large
+      break if arr[j + 1].nil?
+        if arr[j + 1] < arr[j]
+          arr[j],arr[j+1] = arr[j+1],arr[j]
         end
     end
-    i+=1
-  end    
+    i += 1
+  end
   arr
 end
 
-arr = [4,3,78,2,0,2]
+arr = [4, 3, 78, 2, 0, 2]
 
 puts bubble_sort(arr)
 
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,17 +2,14 @@ def bubble_sort_by(arr)
   for i in 0..arr.length
     limit = arr.length - i - 1
     for j in 0..limit
-      break if arr[j+1].nil?
+      break if arr[j + 1].nil? 
       result = yield(arr[j],arr[j+1])
-      if result > 0
-        small = arr[j + 1]
-        large = arr[j]
-        arr[j] = small
-        arr[j + 1] = large
+      next if result !> 0 
+      arr[j],arr[j+1] = arr[j+1],arr[j]
       end
     end
   end
-  arr    
+  arr
 end
 
 arr = ["hi","hello","hey"]
