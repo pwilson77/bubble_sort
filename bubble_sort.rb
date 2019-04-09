859055a3fda90ae44593099a771b782d3bcc0f52
pwilson77 <p.o.yirenkyi@gmail.com>
2019-04-09 09:53:33 +0000

    refactoring...2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,11 @@
 def bubble_sort(arr)
-  i = 0
-  while i < arr.length
-    limit = arr.length - i - 1
-    for j in 0..limit
+  0.upto(arr.length) do |i|
+    0.upto(arr.length-2) do |j|
       break if arr[j + 1].nil?
         if arr[j + 1] < arr[j]
-          arr[j],arr[j+1] = arr[j+1],arr[j]
+          arr[j], arr[j + 1] = arr[j + 1], arr[j]
         end
     end
-    i += 1
   end
   arr
 end
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,10 +1,9 @@
 def bubble_sort_by(arr)
-  for i in 0..arr.length
-    limit = arr.length - i - 1
-    for j in 0..limit
-      break if arr[j + 1].nil? 
+  0.upto(arr.length) do |i|
+    0.upto(arr.length-2) do |j|
+      break if arr[j + 1].nil?
       result = yield(arr[j],arr[j+1])
-      if result > 0 
+      if result > 0
         arr[j],arr[j+1] = arr[j+1],arr[j]
       end
     end
