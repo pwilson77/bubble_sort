ad1fc48edad91cfe68928ff369dd369231535835
pwilson77 <p.o.yirenkyi@gmail.com>
2019-04-09 10:12:21 +0000

    refactoring...3

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,18 @@
 def bubble_sort(arr)
-  0.upto(arr.length) do |i|
-    0.upto(arr.length-2) do |j|
+  0.upto(arr.length) do 
+    0.upto(arr.length - 2) do |j|
       break if arr[j + 1].nil?
-        if arr[j + 1] < arr[j]
-          arr[j], arr[j + 1] = arr[j + 1], arr[j]
-        end
+
+      if arr[j + 1] < arr[j]
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+      end
     end
   end
   arr
 end
 
 arr = [4, 3, 78, 2, 0, 2]
-
 puts bubble_sort(arr)
-
 
 
 
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,19 +1,20 @@
 def bubble_sort_by(arr)
-  0.upto(arr.length) do |i|
-    0.upto(arr.length-2) do |j|
+  0.upto(arr.length) do 
+    0.upto(arr.length - 2) do |j|
       break if arr[j + 1].nil?
-      result = yield(arr[j],arr[j+1])
+
+      result = yield(arr[j],arr[j + 1])
       if result > 0
-        arr[j],arr[j+1] = arr[j+1],arr[j]
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
       end
     end
   end
   arr
 end
 
-arr = ["hi","hello","hey"]
+arr = %w[hi hello hey]
 
 puts bubble_sort_by(arr) {
-  |first,second| 
+  |first, second|
   first.length - second.length
 }
